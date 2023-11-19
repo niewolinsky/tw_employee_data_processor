76002691de75e01a1e98675d6af2b8046cccb211
niewolinsky <niewolinski@protonmail.com>
2023-11-19 18:56:17 +0100

    add redis caching

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -14,6 +15,24 @@ import (
 func (app *application) hdlGetUniqueEmails(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	cachedResponse, err := app.redisClient.Get(context.TODO(), "uniqueDomainsSorted").Result()
+	if err != nil {
+		switch {
+		case (err.Error() == "redis: nil"):
+			fmt.Println("empty cache")
+		default:
+			slog.Error("cache error", "MESSAGE", err)
+		}
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(cachedResponse))
+		if err != nil {
+			utils.ServerErrorResponse(w, r, err)
+		}
+		return
+	}
 
 	req := &pb.ListEmployeesRequest{
 		Limit:  10000,
@@ -34,12 +53,13 @@ func (app *application) hdlGetUniqueEmails(w http.ResponseWriter, r *http.Reques
 
 	uniqueDomainsSorted := customerimporter.CountDomainsConcurrent(domainProviders)
 
-	fmt.Printf("%v", uniqueDomainsSorted)
-
-	// Write the response as JSON
-	err = utils.WriteJSON(w, http.StatusOK, utils.Wrap{"uniqueDomainsSorted": uniqueDomainsSorted}, nil)
+	jsonData, err := utils.WriteJSONCache(w, http.StatusOK, utils.Wrap{"uniqueDomainsSorted": uniqueDomainsSorted}, nil)
 	if err != nil {
 		utils.ServerErrorResponse(w, r, err)
-		return
+	}
+
+	err = app.redisClient.Set(context.TODO(), "uniqueDomainsSorted", jsonData, time.Hour*24).Err()
+	if err != nil {
+		slog.Error("failed caching response", "MESSAGE", err)
 	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -45,6 +45,25 @@ func WriteJSONBytes(w http.ResponseWriter, status int, data Wrap, headers http.H
 	return json, nil
 }
 
+func WriteJSONCache(w http.ResponseWriter, status int, data Wrap, headers http.Header) ([]byte, error) {
+	json, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+
+	json = append(json, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(json)
+
+	return json, nil
+}
+
 func ReadJSON(w http.ResponseWriter, r *http.Request, source interface{}) error {
 	requestLimit := 5242880
 	r.Body = http.MaxBytesReader(w, r.Body, int64(requestLimit))
